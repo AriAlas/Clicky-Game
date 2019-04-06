58d31fbb49f316192f1ca6cf810cbf4e07a7b449
arieru <arieru@gmail.com>
2019-04-06 14:31:00 -0400

    Updates

---
+++ precache-manifest.48ac9c4d59df309aebc4d47192bce897.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "265538ab0e4606d350e6",
+    "url": "/clicky-game/static/js/runtime~main.27874bd3.js"
+  },
+  {
+    "revision": "237ba8269a686b0c4172",
+    "url": "/clicky-game/static/js/main.99cdc93a.chunk.js"
+  },
+  {
+    "revision": "dd0ed99a380f8d225736",
+    "url": "/clicky-game/static/js/2.884c28c3.chunk.js"
+  },
+  {
+    "revision": "237ba8269a686b0c4172",
+    "url": "/clicky-game/static/css/main.04d6a5e3.chunk.css"
+  },
+  {
+    "revision": "6b1a0fa968c11f92527e0094151f3188",
+    "url": "/clicky-game/index.html"
+  }
+];
